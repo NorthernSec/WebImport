e6e0682372773d88b3ef4215cff4dcf00edee8e6
Pidgey <pieterjan.moreels@gmail.com>
2021-07-30 21:57:30 +0200

    version change + credits

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
 from setuptools import setup
 
 setup(name='webimport',
-      version='0.1.0',
+      version='0.1.1',
       description='Enables python to import modules over a http connection',
       author='Pieter-Jan Moreels',
       url='https://github.com/NorthernSec/webimport',
       packages=['webimport'],
       license="GNU GPL",
 )
+# Thanks to:
+#  * Mark Baggett (Github: MarkBaggett)
